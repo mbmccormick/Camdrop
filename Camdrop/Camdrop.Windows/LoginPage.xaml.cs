8400e2ff87275201d90bd14dd856f8fe3f8268a8
Matt McCormick <matt@mbmccormick.com>
2014-09-06 11:26:38 -0700

    Updating all image assets and package manifests.

--- Camdrop/Camdrop.Windows/LoginPage.xaml.cs
+++ Camdrop/Camdrop.Windows/LoginPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,18 +15,97 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace Camdrop
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class LoginPage : Page
     {
         public LoginPage()
         {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            RenderStatusBar();
+
+            LoadData();
+        }
+
+        private void RenderStatusBar()
+        {
+            this.prgStatusBar.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+        }
+
+        private void ShowStatusBar()
+        {
+            this.prgStatusBar.Visibility = Windows.UI.Xaml.Visibility.Visible;
+        }
+
+        private void HideStatusBar()
+        {
+            this.prgStatusBar.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+        }
+
+        private async void LoadData()
+        {
+            ShowStatusBar();
+
+            if (ApplicationData.Current.RoamingSettings.Values.ContainsKey("Username"))
+            {
+                this.txtUsername.Text = ApplicationData.Current.RoamingSettings.Values["Username"] as string;
+            }
+
+            if (ApplicationData.Current.RoamingSettings.Values.ContainsKey("Password"))
+            {
+                this.txtPassword.Password = ApplicationData.Current.RoamingSettings.Values["Password"] as string;
+            }
+
+            if (this.txtUsername.Text.Length > 0 &&
+                this.txtPassword.Password.Length > 0)
+            {
+                btnLogin_Click(this, null);
+            }
+
+            HideStatusBar();
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            ShowStatusBar();
+
+            await App.DropcamClient.LoginLogin((result) =>
+            {
+                Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    HideStatusBar();
+
+                    if (result.status == 0)
+                    {
+                        // login succeeded
+
+                        ApplicationData.Current.RoamingSettings.Values["Username"] = this.txtUsername.Text;
+                        ApplicationData.Current.RoamingSettings.Values["Password"] = this.txtPassword.Password;
+
+                        Frame.Navigate(typeof(MainPage));
+                    }
+                    else
+                    {
+                        // login failed
+
+                        ApplicationData.Current.RoamingSettings.Values.Remove("Username");
+                        ApplicationData.Current.RoamingSettings.Values.Remove("Password");
+
+                        MessageDialog dialog = new MessageDialog(result.status_detail, "Login Failed");
+                        dialog.ShowAsync();
+                    }
+                });
+            }, this.txtUsername.Text, this.txtPassword.Password);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            this.txtUsername.Text = "";
+            this.txtPassword.Password = "";
         }
     }
 }
--- Camdrop/Camdrop.Windows/MainPage.xaml.cs
+++ Camdrop/Camdrop.Windows/MainPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Camdrop.API;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,18 +16,83 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace Camdrop
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        public static ObservableCollection<Camera> VisibleCameras { get; set; }
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            VisibleCameras = new ObservableCollection<Camera>();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (Frame.CanGoBack)
+                Frame.BackStack.RemoveAt(0);
+
+            RenderStatusBar();
+
+            LoadData();
+        }
+        
+        private void RenderStatusBar()
+        {
+            this.prgStatusBar.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+        }
+
+        private void ShowStatusBar()
+        {
+            this.prgStatusBar.Visibility = Windows.UI.Xaml.Visibility.Visible;
+        }
+
+        private void HideStatusBar()
+        {
+            this.prgStatusBar.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+        }
+
+        private async void LoadData()
+        {
+            ShowStatusBar();
+
+            await App.DropcamClient.CamerasGetVisible((result) =>
+            {
+                Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    HideStatusBar();
+
+                    if (result.status == 0)
+                    {
+                        // request succeeded
+
+                        VisibleCameras.Clear();
+
+                        foreach (Camera item in result.items)
+                        {
+                            VisibleCameras.Add(item);
+                        }
+                    }
+                    else
+                    {
+                        // request failed
+
+                        MessageDialog dialog = new MessageDialog(result.status_detail, "Request Failed");
+                        dialog.ShowAsync();
+                    }
+                });
+            });
+
+            HideStatusBar();
+        }
+
+        private void StackPanel_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Camera item = ((FrameworkElement)sender).DataContext as Camera;
+
+            // Frame.Navigate(typeof(CameraPage), item.uuid);
         }
     }
 }
